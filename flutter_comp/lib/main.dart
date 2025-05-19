41b82f7732fecc7130c7371de6f4cbf8db62f00d
noetipo <noe.tipo@gmail.com>
2025-05-19 12:57:49 -0500

    componentes

--- flutter_comp/lib/main.dart
+++ flutter_comp/lib/main.dart
@@ -22,145 +22,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final _formKey = GlobalKey<FormState>();
-  String _email = '';
-  final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-
-  bool _isChecked = false;
-  bool _switchValue = false;
-  int _selectedValue = 1;
-  double _sliderValue = 20.0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('TextField Simple')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Nombre',
-                hintText: 'Escribe tu nombre',
-              ),
-              onChanged: (text) {
-                print('Texto ingresado: $text');
-              },
-            ),
-            SizedBox(height: 10),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Correo Electrónico',
-                      border: OutlineInputBorder(),
-                    ),
-                    onChanged: (value) {_email = value; },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, ingrese un correo';
-                      }
-                      if (!emailRegex.hasMatch(value)) {
-                        return 'Ingrese un correo válido';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        print('Correo ingresado: $_email');
-                      }
-                    },
-                    child: Text('Enviar'),
-                  ),
-                ],
-              ),
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('ListView')),
+        body: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            Container(color: Colors.red, height: 100,
+            child: Column(
               children: [
-                Checkbox(
-                  value: _isChecked,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _isChecked = value ?? false;
-                    });
-                  },
-                ),
-                Text('Acepto los términos y condiciones'),
+                Text("Holas"),
+                ElevatedButton(onPressed: (){}, child: Text("Button"))
               ],
             ),
-
-            Switch(
-              value: _switchValue,
-              onChanged: (bool value) {
-                setState(() {
-                  _switchValue = value;
-                });
-                print('Switch: $_switchValue');
-              },
             ),
-
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ListTile(
-                  title: Text('Opción 1'),
-                  leading: Radio<int>(
-                    value: 1,
-                    groupValue: _selectedValue,
-                    onChanged: (int? value) {
-                      setState(() { _selectedValue = value!; });
-                      print("Seleccionado: $_selectedValue");
-                    },
-                  ),
-                ),
-                ListTile(
-                  title: Text('Opción 2'),
-                  leading: Radio<int>(
-                    value: 2,
-                    groupValue: _selectedValue,
-                    onChanged: (int? value) {
-                      setState(() {_selectedValue = value!; });
-                      print("Seleccionado: $_selectedValue");
-                    },
-                  ),
-                ),
-              ],
-            ),
-
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Slider(
-                    value: _sliderValue,
-                    min: 0,
-                    max: 100,
-                    divisions: 10,
-                    label: _sliderValue.round().toString(),
-                    onChanged: (double value) {
-                      setState(() {_sliderValue = value;});
-                      print("Valor: $_sliderValue");
-                    },
-                  ),
-                  Text('Valor seleccionado: ${_sliderValue.round()}'),
-                ],
-              ),
-            ),
-
+            Container(color: Colors.green, height: 100),
+            Container(color: Colors.blue, height: 100),
+            Container(color: Colors.orange, height: 100),
+            Container(color: Colors.purple, height: 100),
           ],
-        )
-
+        ),
       ),
     );
   }
+
 }
